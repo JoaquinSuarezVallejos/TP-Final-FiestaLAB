b2c691cbb9dedc57bac885de8ac0f15171581396
JoacoSV <joaquin.suarezvallejos@gmail.com>
2022-09-11 15:28:25 -0300

    Update V2

---
+++ Assets/New Scripts/LogicaObjetivos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LogicaObjetivos : MonoBehaviour
+{
+    [SerializeField] int[] numDeObjetivos = new int[13];
+    [SerializeField] TextMeshProUGUI textoMision;
+    [SerializeField] GameObject inticon, EText;
+    [SerializeField] int cantidadObjetivos;
+    void Start()
+    {
+        cantidadObjetivos = numDeObjetivos.Length;
+        cantidadObjetivos = GameObject.FindGameObjectsWithTag("Objetivo").Length;
+        textoMision.text = "Consigue  todos  los  objetos  para  armar  una  fiesta " + "\n Restantes: " + cantidadObjetivos;
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Objetivo")
+        {
+            inticon.SetActive(true);
+            EText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(col.transform.gameObject);
+                cantidadObjetivos--;
+                textoMision.text = "Consigue  todos  los  objetos  para  armar  una  fiesta " + "\n Restantes: " + cantidadObjetivos;
+
+                inticon.SetActive(false);
+                EText.SetActive(false);
+            }
+
+            if (cantidadObjetivos <= 0)
+            {
+                textoMision.text = "Regresa con el NPC";
+            }
+        }
+   }
+
+   void OnTriggerExit(Collider col)
+   {
+        if (col.gameObject.tag == "Objetivo")
+        {
+            inticon.SetActive(false);
+            EText.SetActive(false);
+        }
+   }
+}
